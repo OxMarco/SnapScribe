f2fe7884cd087b96a2c3881f2a60aedd2edd7b9c
OxMarco <>
2024-12-03 10:25:33 +0100

    live updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: "pics.snapscrie",
   appName: "SnapScribe",
   webDir: "dist",
+  plugins: {
+    LiveUpdates: {
+      appId: '36ebbe0a',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
